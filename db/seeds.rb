88f0a4d3a368ebea813c47fcbea14549be9c3e86
jerome <jeromeabelida19@gmail.com>
2024-10-08 00:33:52 +0800

    added: validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create([{name: "xiaomi", description: "cellphone", quantity: 2, price: 19.75, available: true, release_at: DateTime.now - 3, expiry_date: DateTime.now + 15, discount: 0.05},
+
+{name: "realme", description: "cellphone", quantity: 5, price: 21.00, available: false, release_at: DateTime.now - 10, expiry_date: DateTime.now + 20, discount: 0.15},
+
+{name: "vivo", description: "cellphone", quantity: 3, price: 20.50, available: true, release_at: DateTime.now - 5, expiry_date: DateTime.now + 25, discount: 0.10}
+])
