898d1c87d56c3a4e9835dea4c193f69de1d45770
jerome <jeromeabelida19@gmail.com>
2024-10-09 22:22:42 +0800

    removing unnecessary lines in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,12 +7,10 @@ class ProductsController < ApplicationController
     elsif search_products.present?
       search_products
     end
-    
   end
 
   def new
     @product = Product.new 
-    
   end
 
   def create
@@ -49,12 +47,31 @@ class ProductsController < ApplicationController
   end
 
   def search_name
-    @products = Product.where(name: "#{params[:name]}") if params[:name].present?
+    @products = Product.where('name LIKE ?', "%#{params[:name]}%") if params[:name].present?
+  end
+
+  def search_by_quantity
+    @products = Product.where(quantity: params[:min_quantity]..params[:max_quantity]) if params[:min_quantity].present? && params[:max_quantity].present? 
+  end
+
+  def search_by_price
+    @products = Product.where(price: params[:min_price]..params[:max_price]) if params[:min_price].present? && params[:max_price].present? 
+  end
+
+  def search_by_availability
+    @products = Product.where(available: params[:available]) if params[:available].present? && params[:available].present? 
   end
 
   def search_products
-    puts params[:name]
-    search_name
-
+    if params[:name].present?
+      search_name
+    elsif params[:min_quantity].present? && params[:max_quantity].present?
+      search_by_quantity
+    elsif params[:min_price].present? && params[:max_price].present?
+      search_by_price
+    elsif params[:available].present?
+      puts params[:available]
+      search_by_availability
+    end
   end
 end
