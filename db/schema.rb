f3ce73d3e1572433cce39c8dc6148b70e6a42625
jerome <jeromeabelida19@gmail.com>
2024-10-10 17:27:23 +0800

    added: migration for users in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_09_150655) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_10_085233) do
   create_table "brand_models", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_brand_models_on_product_id"
+    t.index ["user_id"], name: "index_brand_models_on_user_id"
   end
 
   create_table "products", charset: "utf8mb4", force: :cascade do |t|
@@ -30,6 +32,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_09_150655) do
     t.float "discount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "brand_models", "products"
