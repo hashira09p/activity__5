9374229a9b7be95ba51769191c45c3ba86c81c1a
jerome <jeromeabelida19@gmail.com>
2024-10-12 20:19:25 +0800

    change product owner to brand model owner

--- app/controllers/brand_models_controller.rb
+++ app/controllers/brand_models_controller.rb
@@ -1,18 +1,19 @@
 class BrandModelsController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_product
-  before_action :validate_product_owner, only: [:edit, :update, :destroy]
+  before_action :set_brand_model, only: [:edit, :update ]
+  before_action :validate_brand_model_owner, only: [:edit, :update, :destroy]
 
   def index
-    @brand_models = @product.brand_models
+    @brand_models = @product.brand_models.includes(:user)
   end
 
   def new
-    @brand_model = @product.brand_models.build
+    @brand_model = @product.brand_models.new
   end
 
   def create
-    @brand_model = @product.brand_models.build(brand_model_params)
+    @brand_model = @product.brand_models.new(brand_model_params)
     @brand_model.user = current_user
     if @brand_model.save
       flash[:notice] = 'Model created successfully'
@@ -20,19 +21,36 @@ class BrandModelsController < ApplicationController
     end
   end
 
+  def edit;end
+
+  def update
+    if @brand_model.update(brand_model_params)
+      flash[:notice] = 'Model updated successfully'
+      redirect_to product_brand_models_path(@product)
+    end
+  end
+
+  def destroy
+    
+  end
+
   private
   def set_product
-    @product = Product.find(params[:product_id])
+    @product = Product.find params[:product_id]
+  end
+
+  def set_brand_model
+    @brand_model = @product.brand_models.find(params[:id])
   end
 
   def brand_model_params
     params.require(:brand_model).permit(:name)
   end
 
-  def validate_comment_owner
-    unless @brand_models.user == current_user
+  def validate_brand_model_owner
+    unless @product.user == current_user
       flash[:notice] = 'the comment not belongs to you'
-      redirect_to products_path(@product)
+      redirect_to product_brand_models_path(@product)
     end
   end
 end
